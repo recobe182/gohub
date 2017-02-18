07af75680744fc197e222eb02d95a0a22d721416
Paladetch Usavasereelert <paladetch.u@gmail.com>
2017-02-18 17:11:13 +0900

    Revert "Add ReceiveThenWait to be an option if you want to do a checkpoint right after message is sent to channel."
    This reverts commit 40f356a204f69deefc3f31b9e0be78d2fbb6e579.

--- receiver.go
+++ receiver.go
@@ -17,81 +17,64 @@ const (
 // ReceiverOption can be passed when creating a receiver to set optional configuration.
 type ReceiverOption func(*receiverSetting)
 
-// ReceiverFunc receives messages.
-type ReceiverFunc func(msg ReceiveMessage)
-
 type receiverSetting struct {
-	partitionId     string
-	consumerGroup   string
+	partitionId string
+	consumerGroup string
 	checkPointAfter int
-	prefetchCount   int
-	storageSetting  StorageSetting
+	prefetchCount int
+	storageSetting StorageSetting
 }
 
 // ConsumerGroup returns a ReceiverOption that sets consumer group.
-func ConsumerGroup(s string) ReceiverOption {
-	return func(l *receiverSetting) {
-		l.consumerGroup = s
-	}
-}
+func ConsumerGroup(s string) ReceiverOption { return func(l *receiverSetting) { l.consumerGroup = s } }
 
 // CheckPointAfter returns a ReceiverOption that sets check point after receive a specific amount of messages.
-func CheckPointAfter(i int) ReceiverOption {
-	return func(l *receiverSetting) {
-		l.checkPointAfter = i
-	}
-}
+func CheckPointAfter(i int) ReceiverOption { return func(l *receiverSetting) { l.checkPointAfter = i } }
 
 // PrefetchCount returns a ReceiverOption that sets prefetch count.
-func PrefetchCount(i int) ReceiverOption {
-	return func(l *receiverSetting) {
-		l.prefetchCount = i
-	}
-}
+func PrefetchCount(i int) ReceiverOption { return func(l *receiverSetting) { l.prefetchCount = i } }
 
 // ReceiveMessage is a message struct.
 type ReceiveMessage struct {
 	// Msg is a message body.
-	Msg         string
+	Msg string
 
 	// Offset is a partition offset.
-	Offset      string
+	Offset string
 
 	// SeqNo is a message sequence number.
-	SeqNo       int64
+	SeqNo int64
 
 	PartitionId string
 
 	// Error object is set in case of failure.
-	Error       error
+	Error error
 }
 
 // EVHReceiver is a receiver interface use to receive a message to Azure Event Hub.
 type EVHReceiver interface {
 	// Receive a message through out channel.
-	// This is a block operation and perform checkpoint right after message is read from this channel.
 	Receive(out chan <- ReceiveMessage)
-
-	// Receive a message through out channel.
-	// This is a block operation. Checkpoint will be performed after recvFunc is done.
-	ReceiveThenWait(recvFunc ReceiverFunc)
 }
 
 // EVHReceiver implementation.
 type evhReceiver struct {
-	receiver        electron.Receiver
-	hub             string
-	consumerGroup   string
-	partitionId     string
+	receiver electron.Receiver
+	hub string
+	consumerGroup string
+	partitionId string
 	checkpointAfter int
 	checkpointCount int
 
-	p               partition
+	p partition
 }
 
 func (r*evhReceiver) Receive(out chan <- ReceiveMessage) {
 	for {
-		if rm, err := r.receiver.Receive(); err == nil {
+		if rm, err := r.receiver.Receive(); err != nil {
+			out <- ReceiveMessage{Error: err}
+			break
+		} else {
 			ret := ReceiveMessage{
 				Msg: string(rm.Message.Body().(amqp.Binary)),
 				Offset: rm.Message.Annotations()[offset].(string),
@@ -107,44 +90,7 @@ func (r*evhReceiver) Receive(out chan <- ReceiveMessage) {
 			}).Debug("Received")
 			out <- ret
 			r.checkpoint(&ret)
-		} else {
-			out <- ReceiveMessage{Error: err}
-			break
 		}
-	}
-}
-
-func (r*evhReceiver) ReceiveThenWait(recvFunc ReceiverFunc) {
-	for {
-		msg := r.getMessage()
-		if msg.Error == nil {
-			recvFunc(msg)
-			r.checkpoint(&msg)
-		} else {
-			recvFunc(msg)
-			break
-		}
-	}
-}
-
-func (r*evhReceiver) getMessage() ReceiveMessage {
-	if rm, err := r.receiver.Receive(); err == nil {
-		ret := ReceiveMessage{
-			Msg: string(rm.Message.Body().(amqp.Binary)),
-			Offset: rm.Message.Annotations()[offset].(string),
-			SeqNo: rm.Message.Annotations()[sequenceNumber].(int64),
-			PartitionId: r.partitionId,
-			Error: nil,
-		}
-		log.WithFields(log.Fields{
-			"Partition": r.partitionId,
-			"Offset": ret.Offset,
-			"Seq No": ret.SeqNo,
-			"Message": ret.Msg,
-		}).Debug("Received")
-		return ret
-	} else {
-		return ReceiveMessage{Error: err}
 	}
 }
 
